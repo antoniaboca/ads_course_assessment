d2ba8f72291ff491479eb78147987f25b7a65e21
Antonia Boca <aib36@cam.ac.uk>
2021-11-30 20:46:24 +0000

    fix another bug

--- fynesse/access_scripts/schemas.py
+++ fynesse/access_scripts/schemas.py
@@ -1,6 +1,6 @@
 DATABASE_CREATE = """
     SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
-    SET time_zone = +00:00;
+    SET time_zone = '+00:00';
 
     CREATE DATABASE IF NOT EXISTS 'property_prices' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 """
